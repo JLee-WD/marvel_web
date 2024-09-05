9af4aaaa3dcd74c793ff81f97ff8c4c6974ac820
Justin Lee <justin.lee26@gmail.com>
2024-09-05 15:37:21 +1000

    Added responsiveness to home

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -15,9 +15,11 @@ const Card = ({reference, key, item}) => {
   return (
     <Link href={`/characters/${id}`}>
       <div ref={reference} key={key} style={gradientStyle} className={styles.card__container}>
-        <h2 className={styles.card__title}>{name}</h2>
         <div className={styles.card__imageContainer}>
           <img className={styles.card__image} src={thumbnailImg} alt={name} />
+        </div>
+        <div className={styles.card__titleContainer}>
+          <h2 className={styles.card__title}>{name}</h2>
         </div>
         <div className={styles.card__descriptionContainer}>
           <p className={styles.card__description}>{description}</p>
--- src/components/ReadingList/index.js
+++ src/components/ReadingList/index.js
@@ -7,11 +7,11 @@ const ReadingList = ({comics}) => {
   return (
     <div className={styles.readingList__container}>
       Reading List: {comics ? comics.length : 0}
-      <div className={styles.readingList__list}>
+      <ol className={styles.readingList__list}>
         {comics && comics.map(comic => 
-          <p key={comic.Comic.id}>{comic.Comic.title}</p>
+          <li key={comic.Comic.id}>{comic.Comic.title}</li>
         )}
-      </div>
+      </ol>
     </div>
   )
 }
